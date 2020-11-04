12e733cfa60e5e64d0d7df27165d4a5cf484b244
jared <jtlss14@gmail.com>
2020-11-04 14:55:43 +0000

    typo

--- task1.py
+++ task1.py
@@ -31,3 +31,4 @@ print("")
 # Now everyone has different attributes making them all special in their own way
 for cat in (Molly, Felix, Smudge):
     print( f"{cat.name} has whiskers: {cat.has_whiskers} and has a jumping ability of {cat.jumping_ability}")
+    print(cat.meow())
